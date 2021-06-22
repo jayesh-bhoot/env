d9d0003b6365ffa56e144082231f4c23ba857c70
Jayesh Bhoot <jayesh@bhoot.sh>
2021-06-22 17:45:26 +0530

    credit the original source

--- nixos/etc/nixos/modules/current-package-list.nix
+++ nixos/etc/nixos/modules/current-package-list.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 
+# Thanks to: https://old.reddit.com/r/NixOS/comments/fsummx/how_to_list_all_installed_packages_on_nixos/fm45htj/
 {
   environment.etc."current-system-packages".text = let
     packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
