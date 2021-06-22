c1a52577831937d9b3bc3c871e5179fee282de30
Jayesh Bhoot <jayesh@bhoot.sh>
2021-06-22 22:49:49 +0530

    Stop overwriting the default leader key (\). Use <space> directly in mappings. Restructure the configuration.

--- nixpkgs/.config/nixpkgs/modules/neovim.nix
+++ nixpkgs/.config/nixpkgs/modules/neovim.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/neovim.nix
 {
   programs.neovim = {
     enable = true;
@@ -8,85 +9,105 @@
     vimdiffAlias = true;
     plugins = with pkgs.vimPlugins; [ 
       vim-nix
+      papercolor-theme
     ];
     extraConfig = ''
-      " Use <space> as leader. There are two solutions:
-      " 1. nullify <space> usage, and then assign <space> directly as the leader.
-      " nnoremap <SPACE> <Nop>
-      " let mapleader=" "
-      " However, with the above solution, when `set showcmd` is on,
-      " ideavim does not display anything for space in the bottom right command corner.
-      " In contrast, neovim displays <20> for space.
-      
-      " 2. A better solution is to retain '\' as the leader, and map <Space> to <leader>,
-      " so that '\' will show up in the command corner when leader is invoked, i.e., <space> or \ is pressed.
-      " I don't have any use for the default leader key '\' anyway.
-      " Also, I don't have to unmap space first.
-      map <Space> <leader>
-      
+      "{ navigation and search
+        " use j and k to move across the visible, soft-wrapped screen lines, instead of physical lines.
+        " this solution also ensures that jumping around using the relative linenumbers keep working.
+        " use gj and gk to move across physical lines instead.
+        " https://stackoverflow.com/a/21000307/663911
+        nnoremap <expr> k (v:count == 0 ? 'gk' : 'k')
+        nnoremap <expr> j (v:count == 0 ? 'gj' : 'j')
+
+        " Retain the default case-sensitive search.
+        " Use the escape sequence \c for a case-insensitive search.
+        " eg: /esc will ignore Esc, but /\cesc or /esc\c will find it.
+        " IF ignorecase is true, then \C (capital C) can perform an explicit case-sensitive search.
+        " https://stackoverflow.com/a/2287449
+        set noignorecase 
+        set incsearch  
+
+        " keep n lines above/below cursor when scrolling
+        set scrolloff=1  
+
+        " Hit `%` on `if` to jump to `else`.
+        runtime macros/matchit.vim
+      "}
+
+
+      "{ buffer management
+        set ruler
+        set number
+        set relativenumber
+
+        set showmode
+        set showcmd
+        set mouse=nvic
+
+        set hidden  
+        nnoremap gb :ls<CR>:b<Space>
+
+        nnoremap <space>w :w<CR>
+        nnoremap <space>q :q<CR>
+        nnoremap <space>wq :wq<CR>
+        nnoremap <space>qq :q!<CR>
+      "}
+
+
+      "{ formatting 
+        " https://old.reddit.com/r/vim/wiki/tabstop
+        " https://tedlogan.com/techblog3.html
+        " https://old.reddit.com/r/vim/comments/1yfdds/confused_about_cin_smartindent_autoindent_etc/cfk0r70/
+
+        " 1 column = 1 space
+
+        " The following configuration ensures that:
+        " 1. the width of the tab-character is retained at 8 spaces, so as not to mess with the default configuration.
+        set tabstop=8
+
+        " 2. spaces are inserted instead of tab-character when tab-key is pressed. The tab-character itself is still accessible with Ctrl-V<Tab> key sequence in insert mode.
+        set expandtab
+
+        " 3. indentation level (the number of columns the cursor moves when one of the tab-key, backspace-key, <<, >> is pressed) is set to 4 columns.
+        set softtabstop=4 " for tab-key and backspace-key
+        set shiftwidth=4  " for << and >>
+
+        " 4. minimal auto-indentation is sanely configured even in buffers with no associated filetype
+        set autoindent
+
+        " 5. vim is configured to detect the filetype of the files you edit and set the appropriate indentation rules automatically.
+        filetype plugin indent on
+
+        " 6. smartindent is turned off. smartindent is an old feature that was meant as a "smarter" context-specific companion to plain autoindent. It is now superseded by both cindent and filetype-specific indentexpr so there's no good reason to set it on.
+        set nosmartindent
+
+        " 7. backspace-key can erase auto-indentation, line-break, and start of insert (what's this?) respectively
+        set backspace=indent,eol,start
+
+        " 8. default simple syntax highlighting is turned on . This is usually reset by a colour scheme.
+        syntax on
+        set background=light
+        colorscheme PaperColor
+
+        " Don't activate ex-mode on Q. Remap Q to formatting the current line.
+        map Q gq 
+      "}
+
+      "{ editing
+        nnoremap <space>s :s/
+        nnoremap <space>S :%s/
+        vnoremap <space>s :s/
+        vnoremap <space>S :%s/
+
+        inoremap jj <Esc>
+
+        " always use system clipboard. ^= means *prepend* to the existing clipboard value. 
+        set clipboard^=unnamed,unnamedplus
+      "}
+
       nnoremap , :
       vnoremap , :
-
-      inoremap jj <Esc>
-      
-      nnoremap <leader>w :w<CR>
-      nnoremap <leader>q :q<CR>
-
-      nnoremap gb :ls<CR>:b<Space>
-
-      nnoremap <leader>s :s/
-      nnoremap <leader>S :%s/
-      vnoremap <leader>s :s/
-      vnoremap <leader>S :%s/
-
-      tnoremap <Esc> <C-\><C-n>
-
-      " use j and k to move across the visible, soft-wrapped screen lines, instead of physical lines.
-      " this solution also ensures that jumping around using the relative linenumbers keep working.
-      " use gj and gk to move across physical lines instead.
-      " https://stackoverflow.com/a/21000307/663911
-      noremap <expr> k (v:count == 0 ? 'gk' : 'k')
-      noremap <expr> j (v:count == 0 ? 'gj' : 'j')
-
-      " Better to retain the default case-sensitive search,
-      " and use the escape sequence \c instead for a case-insensitive search.
-      " eg: /esc will ignore Esc, but /\cesc or /esc\c will find it.
-      " IF ignorecase is true, then \C (capital C) can perform an explicit case-sensitive search.
-      " https://stackoverflow.com/a/2287449
-      " set ignorecase 
-      set incsearch
-
-
-      set nocompatible
-
-      set showmode
-      set showcmd
-
-      set number
-      set relativenumber
-      
-      " this set always uses spaces instead of tabs => no mix of tabs and spaces.
-      " check :h tabstop for hints on this set of config
-      set tabstop=4
-      set shiftwidth=4
-      set expandtab
-      set smarttab
-      set smartindent
-      set autoindent
-
-      set mouse=nvic
-      
-      " always use system clipboard. ^= makes a cross-platform solution.
-      set clipboard^=unnamed,unnamedplus
-
-      set scrolloff=1  " keep n lines above/below cursor when scrolling
-
-      map Q gq " Don't use Ex mode, use Q for formatting.
-
-      if has("gui_running")
-        set guifont=Consolas:h18
-        set linespace=3
-      endif
     '';
   };
 }
