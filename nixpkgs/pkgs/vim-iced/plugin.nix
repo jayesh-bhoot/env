5baa5cde770dab71ec0970ca9621bce315a54680
Jayesh Bhoot <jayesh@bhoot.sh>
2021-09-16 01:20:07 +0530

    cleanup

--- nixpkgs/pkgs/vim-iced/plugin.nix
+++ nixpkgs/pkgs/vim-iced/plugin.nix
@@ -10,12 +10,4 @@ vimUtils.buildVimPlugin {
     sha256 = "1klvq511zc1y8rbjizhc3m0hx3a0ydcjv7wz1s74jx8vnfw24w5n";
   };
   meta.homepage = "https://github.com/liquidz/vim-iced";
-
-  postInstall = ''
-    echo "running YO YO"
-    echo $out
-    echo $target
-    # mkdir $out/bin
-    # ln -s $out/share/vim-plugins/vim-iced/bin/iced $out/bin/iced
-  '';
 }
