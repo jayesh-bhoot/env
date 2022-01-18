67266a44bb556b0c18789840f93ddb970fee3af6
Jayesh Bhoot <jayesh@bhoot.sh>
2022-01-18 10:21:34 +0530

    Put nix indicator on the status line of the prompt to allow more space on the prompt line

--- dotfiles/bash/.bashrc.d/prompt.bash
+++ dotfiles/bash/.bashrc.d/prompt.bash
@@ -1,16 +1,8 @@
-# bash_or_nixed_bash() {
-#     if [ -n "$IN_NIX_SHELL" ]; then 
-#         echo "nix | bash"; 
-#     else 
-#         echo "bash"; 
-#     fi
-# }
-
-prompt_or_nixed_prompt() {
+bash_or_nixed_bash() {
     if [ -n "$IN_NIX_SHELL" ]; then 
-        echo "nix $"; 
+        echo "nix-shell | bash"; 
     else 
-        echo "$"; 
+        echo "bash"; 
     fi
 }
 
@@ -19,5 +11,5 @@ source ~/.nix-profile/share/bash-completion/completions/git-prompt.sh
 start_green_text="\[\e[32m\]"
 end_green_text="\[\e[00m\]"
 
-export PS1="\n${start_green_text}bash | \u@\h | \w \$(__git_ps1 '(%s)') ${end_green_text} \n\$(prompt_or_nixed_prompt) "
+export PS1="\n${start_green_text}\$(bash_or_nixed_bash) | \u@\h | \w \$(__git_ps1 '(%s)') ${end_green_text} \n$ "
 
