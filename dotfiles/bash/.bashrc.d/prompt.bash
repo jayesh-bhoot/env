17d41bc4b41d9f94959cd7c0a1c0ca0cd069c529
Jayesh Bhoot <jayesh@bhoot.sh>
2022-01-21 17:59:12 +0530

    Remove user and hostname from bash prompt to reduce its length

--- dotfiles/bash/.bashrc.d/prompt.bash
+++ dotfiles/bash/.bashrc.d/prompt.bash
@@ -70,10 +70,10 @@ prompt() {
         # So PS1="\[\033[37m\]bash$", while printf "\033[37mbash$"
 
         local colour_sequence="\033[${blue_fg};${light_gray_bg}m"
-        printf "\n${colour_sequence}${user_host} | ${current_dir} ${git_prompt} ${reset_colour_sequence} \nnix $ "
+        printf "\n${colour_sequence}nix bash | ${current_dir} ${git_prompt} ${reset_colour_sequence} \n$ "
     else 
         local colour_sequence="\033[${red_fg};${light_gray_bg}m"
-        printf "\n${colour_sequence}${user_host} | ${current_dir} ${git_prompt} ${reset_colour_sequence} \n$ "
+        printf "\n${colour_sequence}bash | ${current_dir} ${git_prompt} ${reset_colour_sequence} \n$ "
     fi
 }
 
