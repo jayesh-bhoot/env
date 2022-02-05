8251889f01851c95f0e3c203315c3f7ae75b355d
Jayesh Bhoot <jayesh@bhoot.sh>
2022-02-05 13:24:09 +0530

    Ignore vim's *.swp files in fzf invocations

--- dotfiles/bash/.bashrc.d/fzf.bash
+++ dotfiles/bash/.bashrc.d/fzf.bash
@@ -1,7 +1,8 @@
 # TODO: use if/case to source files based on the OS
 
-export FZF_CTRL_T_COMMAND="find . -path '*/\.git' -prune -o -type f -print -o -type l -print | sed 's:^..::'"
-export FZF_DEFAULT_COMMAND="find . -path '*/\.git' -prune -o -type f -print -o -type l -print | sed 's:^..::'"
+# `find . [test action] -operator [test action]` is used as a convention to keep the command readable.
+# This is why `-name '*.swp' -prune` is used instead of `! -name '*.swp' -a (rest of it)` even if *.swp is not a dir.
+export {FZF_DEFAULT_COMMAND,FZF_CTRL_T_COMMAND}="find . -name '*.swp' -prune -o -path '*/\.git' -prune -o -type f -print -o -type l -print | sed 's:^..::'"
 export FZF_DEFAULT_OPTS="--exact"
 
 # nix
