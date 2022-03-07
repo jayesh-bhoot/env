856456336ab4899d3f21ad43c10afa8d55ede984
Jayesh Bhoot <jayesh@bhoot.sh>
2022-03-07 10:22:08 +0530

    Add sublime and macvim to PATH for macOS

--- dotfiles/bash/.bashrc.d/env-vars.sh
+++ dotfiles/bash/.bashrc.d/env-vars.sh
@@ -5,3 +5,7 @@ export EDITOR="vim"
 export VISUAL="vim"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
+
+# TODO: Not a cross-platform solution
+export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+export PATH="/Applications/MacVim.app/Contents/bin:$PATH"
