351792cb587e78349a0f07ffe9e9b25d3aa33c0d
Jayesh Bhoot <jayesh@bhoot.sh>
2022-03-08 14:23:17 +0530

    Configure sublime text as VISUAL and EDITOR

--- dotfiles/bash/.bashrc.d/env-vars.sh
+++ dotfiles/bash/.bashrc.d/env-vars.sh
@@ -6,4 +6,5 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=true
 # TODO: Not a cross-platform solution
 export PATH="/Applications/MacVim.app/Contents/bin:$PATH"
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
-export EDITOR='subl -w'
+export EDITOR='subl -n -w'
+export VISUAL='subl -n -w'
