e1c5874d10d077d2bb340ccbfd5404f4dfd0df45
Jayesh Bhoot <jayesh@bhoot.sh>
2022-03-08 14:20:23 +0530

    Use sublime text as EDITOR

--- dotfiles/bash/.bashrc.d/env-vars.sh
+++ dotfiles/bash/.bashrc.d/env-vars.sh
@@ -1,11 +1,9 @@
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
-export EDITOR="vim"
-export VISUAL="vim"
-
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
 
 # TODO: Not a cross-platform solution
-export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
 export PATH="/Applications/MacVim.app/Contents/bin:$PATH"
+export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+export EDITOR='subl -w'
