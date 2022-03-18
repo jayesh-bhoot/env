e12572465c9e8cf45c08e64d16edd960f6a05904
Jayesh Bhoot <jayesh@bhoot.sh>
2022-03-18 14:42:48 +0530

    Expose sublime merge binary in PATH

--- dotfiles/bash/.bashrc.d/env-vars.sh
+++ dotfiles/bash/.bashrc.d/env-vars.sh
@@ -6,5 +6,6 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=true
 # TODO: Not a cross-platform solution
 export PATH="/Applications/MacVim.app/Contents/bin:$PATH"
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+export PATH="/Applications/Sublime Merge.app/Contents/SharedSupport/bin:$PATH"
 export EDITOR='subl -n -w'
 export VISUAL='subl -n -w'
