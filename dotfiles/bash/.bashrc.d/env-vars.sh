bc21a2d0df32d0a3fcef2169e226979b97076602
Jayesh Bhoot <jayesh@bhoot.sh>
2022-04-26 14:12:44 +0530

    Remove sublime, macvim, and sublime merge from PATH

--- dotfiles/bash/.bashrc.d/env-vars.sh
+++ dotfiles/bash/.bashrc.d/env-vars.sh
@@ -7,6 +7,6 @@ export EDITOR='kak'
 export VISUAL='kak'
 
 # TODO: Not a cross-platform solution
-export PATH="/Applications/MacVim.app/Contents/bin:$PATH"
-export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
-export PATH="/Applications/Sublime Merge.app/Contents/SharedSupport/bin:$PATH"
+# export PATH="/Applications/MacVim.app/Contents/bin:$PATH"
+# export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+# export PATH="/Applications/Sublime Merge.app/Contents/SharedSupport/bin:$PATH"
